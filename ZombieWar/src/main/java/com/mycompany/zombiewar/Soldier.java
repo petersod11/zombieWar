2f830bd388113615c5746744945e8e647a7e3f6e
shado <shado@DESKTOP-7CGLKJE>
2021-06-08 13:34:01 -0600

    ZombieWar release 1

---
+++ ZombieWar/src/main/java/com/mycompany/zombiewar/Soldier.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.zombiewar;
+
+public class Soldier extends Survivor {
+     public static int count = 0;
+    
+    public Soldier (){
+        setName("Soldier " + count++);
+        setHealth(100);
+        setDamage(10);
+    }
+}
